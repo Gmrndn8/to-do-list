21737eca5dcc2dac2d69f0e59609ee117c457ad3
Gideon Kipng'etich <gideonmrndn8@gmail.com>
2017-05-09 18:51:54 +0300

    Final draft made

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function toDo(objective, duration) {
   this.objective = objective;
   this.time = duration;
 }
+
+toDo.prototype.completeobjective = function() {
+  return this.objective + " " + this.time;
+}
 //UI logic
 $(document).ready(function() {
   $("form#new-objective").submit(function(event) {
@@ -11,9 +15,13 @@ $(document).ready(function() {
     var inputtedObjective = $("input#new-objective").val();
     var inputtedDuration = $("input#new-duration").val();
 
-    var newtoDo = new toDo(inputtedObjective, inputtedDuration);
+    var newToDo = new toDo(inputtedObjective, inputtedDuration);
 
-    $("ul#all").append("<li><span ='objective'>" + inputtedObjective + " - " + inputtedDuration + "</span></li>")
-    
+    $("ul#all").append("<li><span class='objective'>" + inputtedObjective + " - " + inputtedDuration + "</span></li>")
+    $(".objective").last().click(function() {
+      $("ul#completed").append("<li>" + newToDo.completeobjective() + "</li>");
+    });
+    $("input#new-objective").val("");
+    $("input#new-duration").val("");
   })
 });
